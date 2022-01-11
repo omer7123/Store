36b9ed0d335dc3c9f0e6c3d458de58d7243d4a5b
omer7123 <ilyafomin125@gmail.com>
2022-01-12 00:53:34 +0600

    add navigate to Sign

--- app/src/main/java/com/ripalay/store/ui/start/StartFragment.kt
+++ app/src/main/java/com/ripalay/store/ui/start/StartFragment.kt
@@ -5,13 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.ripalay.store.R
+import com.ripalay.store.databinding.FragmentStartBinding
+import com.ripalay.store.ui.registration.SignUpFragment
+
 class StartFragment : Fragment() {
+    private lateinit var binding: FragmentStartBinding
+    private lateinit var navController: NavController
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        binding = FragmentStartBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        navController = findNavController()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.registerBtn.setOnClickListener {
+            navController.navigate(R.id.signUpFragment)
+        }
     }
 }
